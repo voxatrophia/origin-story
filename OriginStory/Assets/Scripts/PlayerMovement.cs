4364e723d78ed31e2c42db95f795192659f07cda
Arthur Palomo <arthurpalomo@gmail.com>
2016-01-30 16:03:08 -0800

    2D Platformer stuff works, including (static) animations for idle, run, jump. Prayer mechanic works, but not hooked up to sequence checker.

---
+++ OriginStory/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,75 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public float speed = 10;
+    public float jumpForce = 800f;
+
+    Rigidbody2D rb;
+    bool facingRight = true;
+
+    private Transform groundCheck;    // A position marking where to check if the player is grounded.
+    const float groundedRadius = .2f; // Radius of the overlap circle to determine if grounded
+    private bool grounded;            // Whether or not the player is grounded.
+    [SerializeField]
+    private LayerMask whatIsGround;                  // A mask determining what is ground to the character
+
+    Animator anim;
+
+    void Start() {
+        groundCheck = transform.Find("GroundCheck");
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+    }
+
+    void FixedUpdate() {
+        grounded = false;
+        //Check for ground
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(groundCheck.position, groundedRadius, whatIsGround);
+        for (int i = 0; i < colliders.Length; i++) {
+            if (colliders[i].gameObject != gameObject) {
+                grounded = true;
+            }
+        }
+        anim.SetBool("Grounded", grounded);
+//        anim.SetFloat("ySpeed", rb.velocity.y);
+
+    }
+    void Update() {
+
+        float move = Input.GetAxis("Horizontal");
+        rb.velocity = new Vector2(move * speed, rb.velocity.y);
+        anim.SetFloat("Speed", Mathf.Abs(move));
+
+        // If the input is moving the player right and the player is facing left...
+        if (move > 0 && !facingRight) {
+            // ... flip the player.
+            Flip();
+        }
+        // Otherwise if the input is moving the player left and the player is facing right...
+        else if (move < 0 && facingRight) {
+            // ... flip the player.
+            Flip();
+        }
+
+        if (Input.GetButtonDown("Jump")) {
+            if (grounded) {
+                grounded = false;
+                anim.SetBool("Grounded", false);
+                rb.AddForce(new Vector2(0f, jumpForce));
+            }
+        }
+    }
+
+    private void Flip() {
+        // Switch the way the player is labelled as facing.
+        facingRight = !facingRight;
+
+        // Multiply the player's x local scale by -1.
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+    }
+
+}
---
+++ OriginStory/Assets/Scripts/Praying.cs
@@ -0,0 +1,128 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Prayer {
+    //Left = 0
+    //Right = 1
+    //Up = 2
+    public int[] sequence;
+
+    public int step { get; set; }
+
+    public bool first { get; set; }
+    public bool second { get; set; }
+    public bool third { get; set; }
+    public bool fourth { get; set; }
+
+    public void Init() {
+        step = 0;
+
+        sequence = new int[4];
+        sequence[0] = 0; //Left
+        sequence[1] = 2; //Up
+        sequence[2] = 1; //Right
+        sequence[3] = 2; //Up
+
+    }
+}
+
+public class Praying : MonoBehaviour {
+    public bool praying;
+    Prayer prayer;
+    Animator anim;
+    Vector3 origScale;
+
+    bool prayFirst;
+
+    void Awake() {
+        anim = GetComponent<Animator>();
+        prayer = new Prayer();
+        prayer.Init();
+    }
+
+    void Update() {
+        //Start Praying
+        if (!praying && Input.GetButton("Pray")) {
+            StartPraying();
+        }
+
+        //Stop Praying
+        if (Input.GetButtonUp("Pray")) {
+            StopPraying();
+        }
+
+        if (praying) {
+            if (Input.GetButtonDown("Left")) {
+                ChangePrayer(0);
+            }
+            if (Input.GetButtonDown("Right")) {
+                ChangePrayer(1);
+            }
+            if (Input.GetButtonDown("Up")) {
+                ChangePrayer(2);
+            }
+
+            //if (Input.GetKeyDown(KeyCode.Alpha1)) {
+            //    Debug.Log("Pressed 1");
+            //    prayFirst = true;
+            //    anim.SetBool("PrayLeft", true);
+            //}
+            //if (prayFirst && Input.GetKeyDown(KeyCode.Alpha2)) {
+            //    Debug.Log("Part 2");
+            //}
+        }
+
+    }
+
+    void StartPraying() {
+        praying = true;
+        anim.SetBool("Praying", true);
+        origScale = transform.localScale;
+        Vector3 theScale = origScale;
+        if (theScale.x < 0) {
+            theScale.x *= -1;
+        }
+        transform.localScale = theScale;
+        Time.timeScale = 0;
+
+    }
+
+    void StopPraying() {
+        ResetPrayer();
+        transform.localScale = origScale;
+        Time.timeScale = 1;
+    }
+
+    void ResetPrayer() {
+        praying = false;
+        prayer.first = false;
+        prayer.second = false;
+        prayer.third = false;
+        prayer.fourth = false;
+        anim.SetBool("Praying", false);
+        anim.SetBool("PrayLeft", false);
+        anim.SetBool("PrayRight", false);
+        anim.SetBool("PrayUp", false);
+    }
+
+    void ChangePrayer(int direction) {
+        switch (direction) {
+            case 0: //Left, remove right and up
+                anim.SetBool("PrayLeft", true);
+                anim.SetBool("PrayRight", false);
+                anim.SetBool("PrayUp", false);
+                break;
+            case 1: //Right, remove left and up
+                anim.SetBool("PrayRight", true);
+                anim.SetBool("PrayLeft", false);
+                anim.SetBool("PrayUp", false);
+                break;
+            case 2:
+                anim.SetBool("PrayUp", true);
+                anim.SetBool("PrayLeft", false);
+                anim.SetBool("PrayRight", false);
+                break;
+        }
+
+    }
+}
